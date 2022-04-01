6d1bc591a887182720733f9c25e18fb909ea122b
unikrubii <gamez.zeta@gmail.com>
2022-04-02 01:14:44 +0700

    to guaca

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sthitiku <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 14:04:26 by sthitiku          #+#    #+#             */
-/*   Updated: 2022/04/01 17:46:29 by sthitiku         ###   ########.fr       */
+/*   Updated: 2022/04/02 01:14:34 by sthitiku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -140,63 +140,63 @@ char	*get_next_line(int fd)
 	return (ret);
 }
 
-int	main(void)
-{
-	int	fd[4];
-	char *ans;
-	fd[0] = open("41_with_nl", O_RDWR);
-	// ans = get_next_line(1000);
-	// printf("%s", ans);
-	// free(ans);
-	ans = get_next_line(fd[0]);
-	printf("fd[0] %s", ans);
-	free(ans);
+// int	main(void)
+// {
+// 	int	fd[4];
+// 	char *ans;
+// 	fd[0] = open("41_with_nl", O_RDWR);
+// 	// ans = get_next_line(1000);
+// 	// printf("%s", ans);
+// 	// free(ans);
+// 	ans = get_next_line(fd[0]);
+// 	printf("fd[0] %s", ans);
+// 	free(ans);
 
-	fd[1] = open("42_with_nl", O_RDWR);
-	// ans = get_next_line(1001);
-	// printf("%s", ans);
-	// free(ans);
-	ans = get_next_line(fd[1]);
-	printf("fd[1] %s", ans);
-	free(ans);
+// 	fd[1] = open("42_with_nl", O_RDWR);
+// 	// ans = get_next_line(1001);
+// 	// printf("%s", ans);
+// 	// free(ans);
+// 	ans = get_next_line(fd[1]);
+// 	printf("fd[1] %s", ans);
+// 	free(ans);
 
-	// fd[2] = open("43_with_nl", O_RDWR);
-	// ans = get_next_line(1002);
-	// printf("%s", ans);
-	// free(ans);
-	// ans = get_next_line(fd[2]);
-	// printf("%s", ans);
-	// free(ans);
+// 	// fd[2] = open("43_with_nl", O_RDWR);
+// 	// ans = get_next_line(1002);
+// 	// printf("%s", ans);
+// 	// free(ans);
+// 	// ans = get_next_line(fd[2]);
+// 	// printf("%s", ans);
+// 	// free(ans);
 
-	// ans = get_next_line(1003);
-	// printf("%s", ans);
-	// free(ans);
-	ans = get_next_line(fd[0]);
-	printf("fd[0] %s", ans);
-	free(ans);
+// 	// ans = get_next_line(1003);
+// 	// printf("%s", ans);
+// 	// free(ans);
+// 	ans = get_next_line(fd[0]);
+// 	printf("fd[0] %s", ans);
+// 	free(ans);
 	
-	// ans = get_next_line(1004);
-	// printf("%s", ans);
-	// free(ans);
-	ans = get_next_line(fd[1]);
-	printf("fd[1] %s", ans);
-	free(ans);
+// 	// ans = get_next_line(1004);
+// 	// printf("%s", ans);
+// 	// free(ans);
+// 	ans = get_next_line(fd[1]);
+// 	printf("fd[1] %s", ans);
+// 	free(ans);
 	
-	// ans = get_next_line(1005);
-	// printf("%s", ans);
-	// free(ans);
-	// ans = get_next_line(fd[2]);
-	// printf("%s", ans);
-	// free(ans);
+// 	// ans = get_next_line(1005);
+// 	// printf("%s", ans);
+// 	// free(ans);
+// 	// ans = get_next_line(fd[2]);
+// 	// printf("%s", ans);
+// 	// free(ans);
 
 	
-	ans = get_next_line(fd[0]);
-	printf("fd[0] %s", ans);
-	free(ans);
-	ans = get_next_line(fd[1]);
-	printf("fd[1] %s", ans);
-	free(ans);
-	// ans = get_next_line(fd[2]);
-	// printf("%s", ans);
-	// free(ans);
-}
+// 	ans = get_next_line(fd[0]);
+// 	printf("fd[0] %s", ans);
+// 	free(ans);
+// 	ans = get_next_line(fd[1]);
+// 	printf("fd[1] %s", ans);
+// 	free(ans);
+// 	// ans = get_next_line(fd[2]);
+// 	// printf("%s", ans);
+// 	// free(ans);
+// }

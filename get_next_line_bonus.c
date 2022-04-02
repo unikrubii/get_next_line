4a9b7faaa26140e66cb79eca3b5ad1baac5e38d8
unikrubii <gamez.zeta@gmail.com>
2022-04-02 17:14:59 +0700

    may be fixed

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sthitiku <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 14:04:26 by sthitiku          #+#    #+#             */
-/*   Updated: 2022/04/02 16:53:12 by sthitiku         ###   ########.fr       */
+/*   Updated: 2022/04/02 17:14:46 by sthitiku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,5 +135,7 @@ char	*get_next_line(int fd)
 		return (NULL);
 	}
 	curr->str = shift_str(curr->str);
+	if (!curr->str)
+		clear_read(readf, fd);
 	return (ret);
 }
